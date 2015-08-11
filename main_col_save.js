d6c68c80d165c6e552546982065cfe8ca486e923
nevraw <nevraw@users.noreply.github.com>
2015-08-11 08:33:27 +0100

    Create main_col_save.js

---
+++ main_col_save.js
@@ -0,0 +1,117 @@
+(function() {
+ loadOptions();
+ buttonHandler();
+})();
+
+function buttonHandler() {
+ var $submitButton = $('#submitButton');
+
+ $submitButton.on('click', function() {
+//  console.log('Submit');
+ var $preset1Checkbox = $('#preset1Checkbox');
+ var $preset2Checkbox = $('#preset2Checkbox');
+
+ if (($preset1Checkbox[0].checked == 1) && ($preset2Checkbox[0].checked == 1)) {
+  alert('Only one preset allowed');
+  return
+ }
+
+  var return_to = getQueryParam('return_to', 'pebblejs://close#');
+  document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+ });
+
+ var $cancelButton = $('#cancelButton');
+
+ $cancelButton.on('click', function() {
+//  console.log('Cancel');
+  var return_to = getQueryParam('return_to', 'pebblejs://close#');
+  document.location = return_to;
+ });
+}
+
+function loadOptions() {
+ var $hourColorPicker = $('#hourColorPicker');
+ var $min5ColorPicker = $('#min5ColorPicker');
+ var $minColorPicker = $('#minColorPicker');
+ var $preset1Checkbox = $('#preset1Checkbox');
+ var $preset2Checkbox = $('#preset2Checkbox');
+
+ console.log('localStorage.preset: ' + localStorage.preset);
+
+ if (localStorage.preset) {
+  if (localStorage.preset == 0) {
+//   console.log('setting both presets to false');
+   $preset1Checkbox[0].checked = false;
+   $preset2Checkbox[0].checked = false;
+  } else if (localStorage.preset == 1) {
+//   console.log('setting preset1 to true and preset2 to false');
+   $preset1Checkbox[0].checked = true;
+   $preset2Checkbox[0].checked = false;
+  } else if (localStorage.preset == 2) {
+//   console.log('setting preset2 to true and preset1 to false');
+   $preset1Checkbox[0].checked = false;
+   $preset2Checkbox[0].checked = true;
+  }
+ }
+
+ if (localStorage.hourColor) {
+  $hourColorPicker[0].value = localStorage.hourColor;
+ }
+ if (localStorage.min5Color) {
+  $min5ColorPicker[0].value = localStorage.min5Color;
+ }
+ if (localStorage.minColor) {
+  $minColorPicker[0].value = localStorage.minColor;
+ }
+} 
+
+function getAndStoreConfigData() {
+ var $hourColorPicker = $('#hourColorPicker');
+ var $min5ColorPicker = $('#min5ColorPicker');
+ var $minColorPicker = $('#minColorPicker');
+ var $preset1Checkbox = $('#preset1Checkbox');
+ var $preset2Checkbox = $('#preset2Checkbox');
+ var $presetValue = 0;
+ var $preset1Value = 0;
+ var $preset2Value = 0;
+
+ if ($preset1Checkbox[0].checked == 1) {
+  $preset1Value = 1;
+ } else if ($preset2Checkbox[0].checked == 1) {
+  $preset2Value = 1;
+ }
+
+ if ($preset1Value == 1) {
+  $presetValue = 1;
+ } else if ($preset2Value == 1) {
+  $presetValue = 2;
+ }
+
+ var options = {
+  hourColor: $hourColorPicker.val(),
+  min5Color: $min5ColorPicker.val(),
+  minColor: $minColorPicker.val(),
+  preset: $presetValue
+ };
+ 
+// console.log('Got options: ' + JSON.stringify(options));
+
+ localStorage.hourColor = options.hourColor;
+ localStorage.min5Color = options.min5Color;
+ localStorage.minColor = options.minColor;
+ localStorage.preset = options.preset;
+
+ return options;
+}
+
+function getQueryParam(variable, defaultValue) {
+ var query = location.search.substring(1);
+ var vars = query.split('&');
+ for (var i = 0; i < vars.length; i++) {
+  var pair = vars[i].split('=');
+  if (pair[0] === variable) {
+   return decodeURIComponent(pair[1]);
+  }
+ }
+ return defaultValue || false;
+}
